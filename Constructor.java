7c94386d06468a586e6bd061336eb7471f1e4e91
Sonu825313 <sonukumarraj1341998@gmail.com>
2023-08-18 15:31:45 +0530

    constructor-types

--- Constructor.java
+++ Constructor.java
@@ -1,4 +1,5 @@
-
+/*
+                         // Non parameterized Constructor
 class Student{
     String name;
     int age;
@@ -8,11 +9,32 @@ class Student{
         System.out.println(this.age);
     }
     //         //  Non-parameterized constructor
-    //  Student(){
-    //     System.out.println("Called Constructor");  // Constructor bydefault create in java
-    //  }
+     Student(){
+        System.out.println("Called Constructor");  // Constructor bydefault create in java
+     }
 
-          //  parameterized constructor
+}
+public class Constructor {
+
+	public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Maddy";
+        s1.age = 21;
+        s1.studentInfo();
+    }
+}
+*/
+/*
+                         // Parameterized Constructor
+class Student{
+    String name;
+    int age;
+
+    public void studentInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+       //  parameterized constructor
     Student(String name, int age){
        this.name = name;
        this.age = age;
@@ -22,10 +44,37 @@ public class Constructor {
 
 	public static void main(String[] args) {
         Student s1 = new Student("Maddy", 25);
-        // s1.name = "Maddy";
-        // s1.age = 21;
         s1.studentInfo();
-        
+    }
+}
+*/
 
+                // Copy Constructor
+class Student{
+    String name;
+    int age;
+
+    public void studentInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+       //  parameterized constructor
+    Student(Student s2){
+       this.name = s2.name;
+       this.age = s2.age;
+     }
+
+     Student(){
+
+     }
+}
+public class Constructor {
+
+	public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Maddy";
+        s1.age = 25;
+        Student s2 = new Student(s1);
+         s2.studentInfo();
     }
 }

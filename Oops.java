f25339b34963b0098e6ed70b63df35b3d965f449
Sonu825313 <sonukumarraj1341998@gmail.com>
2023-08-18 12:54:48 +0530

    first commit

---
+++ Oops.java
@@ -0,0 +1,30 @@
+                        // OOPS Concept in java basic
+                        // Class AND Objects
+class Pen{
+	 String color;
+	 String type; // "Ballpoint, Gel"
+	 
+	 public void write() {
+		 System.out.println("Writing Something");
+	 }
+	 public void printColor() {
+		 System.out.println(this.color );
+	 }
+}
+	
+public class Oops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pen pen1 = new Pen();
+		pen1.color = "Blue";
+		pen1.type = "Ballpoint";
+		
+        Pen pen2 = new Pen();
+        pen2.color = "Black";
+		pen2.type = "Gel";
+
+        pen1.printColor();
+        pen2.printColor();
+	}  
+}
